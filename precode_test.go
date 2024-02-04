8f988daa0fa4bd71a27c030586396b6f6afe02cb
mobiquai <zerg-serg@yandex.ru>
2024-02-04 17:30:43 +0300

    исправления по замечаниям

--- precode_test.go
+++ precode_test.go
@@ -10,43 +10,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое.
-func TestMainHandlerWhenOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+func createResponseRecorder(count, city string) *httptest.ResponseRecorder {
+	req := httptest.NewRequest("GET", "/cafe", nil)
+	query := req.URL.Query()
+	query.Add("count", count)
+	query.Add("city", city)
+	req.URL.RawQuery = query.Encode()
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, responseRecorder.Code, http.StatusOK) // код ответа должен быть 200
+	return responseRecorder
+}
+
+// Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое.
+func TestMainHandlerWhenOk(t *testing.T) {
+	responseRecorder := createResponseRecorder("2", "moscow")
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code) // код ответа должен быть 200, иначе нет смысла продолжать тест
 	assert.NotEmpty(t, responseRecorder.Body.String())     // тело ответа не должно быть пустым
 }
 
 // Город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
 func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=london", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := createResponseRecorder("3", "london")
 
 	expected := "wrong city value"
 
-	assert.NotEqual(t, responseRecorder.Code, http.StatusBadRequest) // если код ответа 400, то ошибка
-	assert.NotEqual(t, responseRecorder.Body.String(), expected)     // если в теле текст "wrong city value", то ошибка
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code) // код ответа должен быть 400
+	assert.Equal(t, expected, responseRecorder.Body.String())     // если в теле текст "wrong city value", то ошибка
 }
 
 // Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 6
-	req := httptest.NewRequest("GET", "/cafe?count=6&city=moscow", nil)
+	totalCount := 4 // Необходимое кол-во для сравнения
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := createResponseRecorder("5", "moscow")
 
-	require.Equal(t, responseRecorder.Code, http.StatusOK) // код ответа должен быть 200
+	require.Equal(t, http.StatusOK, responseRecorder.Code) // код ответа должен быть 200, иначе нет смысла продолжать тест
 
 	retSlyce := strings.Split(responseRecorder.Body.String(), ",") // получаем слайс из строки по разделителю ","
-	assert.Less(t, totalCount, len(retSlyce))                      // Если указано больше, то ошибка
+	assert.Equal(t, totalCount, len(retSlyce))                     // Если указано меньше 4, то ошибка
 }
